bb0fcd882d28e8c72b9a5c8902b4fa4756511e8c
Ayush Malviya <ayush07malviya07@gmail.com>
2026-08-26 01:53:05 +0530

    Data Creation Done

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = FastAPI()
 
 fake_db = [{"name":"Anar","Price":50},{"name":"Banana","Price":30},{"name":"Apple","Price":80}]
 
+class user_input(BaseModel):
+    name:str
+    Price:int
+
 @app.get("/get_all")
 def get_all():
     return {"data":fake_db,"message":"We get all data"}
@@ -12,3 +16,11 @@ def get_all():
 def get_one(item_in:int):
     f_data = fake_db[item_in]
     return {"data":f_data,"message":"Item got it"}
+
+@app.post("/create_data")
+def data_create(data:user_input):
+    data_dict = {}
+    data_dict["name"] = data.name
+    data_dict["Price"] = data.Price
+    fake_db.append(data_dict)
+    return {"message":"Data created successfully!","data":data_dict}
