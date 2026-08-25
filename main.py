108de38132e73d7470081103bcecbe98d6de2e11
Ayush Malviya <ayush07malviya07@gmail.com>
2026-08-26 01:46:07 +0530

    Get request Done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI,HTTPException
-
+from pydantic import BaseModel
 app = FastAPI()
+
+fake_db = [{"name":"Anar","Price":50},{"name":"Banana","Price":30},{"name":"Apple","Price":80}]
+
+@app.get("/get_all")
+def get_all():
+    return {"data":fake_db,"message":"We get all data"}
+
+@app.get("/get_one/{item_in}")
+def get_one(item_in:int):
+    f_data = fake_db[item_in]
+    return {"data":f_data,"message":"Item got it"}
